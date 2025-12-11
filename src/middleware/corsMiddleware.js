30b7293472a51740f2d9b4591543f08d1541a463
firstsecond8833-maker <firstsecond8833@gmail.com>
2025-12-11 08:33:11 +0530

    solved cors error

--- src/middleware/corsMiddleware.js
+++ src/middleware/corsMiddleware.js
@@ -1,8 +1,8 @@
 import cors from 'cors';
 
 const corsOptions = {
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-    credentials: true,
+    origin: '*',
+    credentials: false,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 };
